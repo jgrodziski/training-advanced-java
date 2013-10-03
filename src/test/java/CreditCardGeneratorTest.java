5fdeab9dbc6a48c181144bc54fc2f05d3ff873d1
Jérémie Grodziski <jeremie.github@grodziski.com>
2013-10-03 14:44:40 +0200

    Revert "add balance to credit card"
    This reverts commit ffe5d97a0f050625aa1a951fbd9a156c8d3e78ca.

--- src/test/java/CreditCardGeneratorTest.java
+++ src/test/java/CreditCardGeneratorTest.java
@@ -5,7 +5,10 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Writer;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  *
@@ -24,13 +27,11 @@ public class CreditCardGeneratorTest {
 
         for (String network : size.keySet()) {
             List<List<String>> rows = new ArrayList<List<String>>(500000);
-            Random rnd = new Random();
             for (int i = 0 ;  i < 500000 ; i++) {
                 List<String> row = new ArrayList<String>(2);
                 CreditCardNumber n = CreditCardNumberGenerator.generate(network, size.get(network));
                 row.add(n.getIssuerNetwork());
                 row.add(n.getNumber());
-                row.add(String.valueOf(rnd.nextInt(1000)));
                 rows.add(row);
             }
             CSVWriter.format(w,rows);
